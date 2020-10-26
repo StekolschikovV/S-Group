0a0f2db94b1dca13c6da139342bee91d7d72a491
mk <v.a.stekolschikov@gmail.com>
2020-10-26 11:40:29 +0200

    init auth

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import {LoginComponent} from "./components/login/login.component";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+import {AuthService} from "./services/auth.service.service";
+
+const routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+  ],
+  declarations: [ LoginComponent],
+  providers: [AuthService]
+})
+export class AuthModule {}
---
+++ src/app/auth/services/auth.service.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  constructor() {
+  }
+
+  login() {
+
+  }
+
+  refresh() {
+
+  }
+
+  logout() {
+    
+  }
+}
